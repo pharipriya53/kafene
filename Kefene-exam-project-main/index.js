2c2d1c713e6c1cb089467d001eef5f20feecec2a
pharipriya53 <123175951+pharipriya53@users.noreply.github.com>
2023-03-13 14:37:10 +0530

    Add files via upload

---
+++ Kefene-exam-project-main/index.js
@@ -0,0 +1,17 @@
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username === password && username.length > 1) {
+      alert("Login Successful!");
+      window.localStorage.setItem("loginStatus", true);
+      window.location.href = "orders.html"; 
+    } else {
+      alert("Please enter valid credentials!");
+      window.location.href = "index.html"; 
+  
+  
+  
+      
+    }
+  }
+  
